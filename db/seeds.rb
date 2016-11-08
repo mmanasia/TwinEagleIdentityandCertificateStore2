3c1fed1e3be85eb957f789827fc253773cce5fd0
donnapham <ddpham4@uh.edu>
2016-11-07 18:44:54 -0600

    DonnaPham_Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,22 @@ UserStoreRole.delete_all
 UserStore.delete_all
 
 
+Environment.create(EnvironmentName: 'Environment Sample 1', EnvironmentDescription: 'Environment Description Sample 1')
+Environment.create(EnvironmentName: 'Environment Sample 2', EnvironmentDescription: 'Environment Description Sample 2')
+Environment.create(EnvironmentName: 'Environment Sample 3', EnvironmentDescription: 'Environment Description Sample 3')
+Environment.create(EnvironmentName: 'Environment Sample 4', EnvironmentDescription: 'Environment Description Sample 4')
+Environment.create(EnvironmentName: 'Environment Sample 5', EnvironmentDescription: 'Environment Description Sample 5')
+
+IdentityType.create(IdentityTypeName: 'Identity Type Sample 1', IdentityTypeDescription: 'Identity Type Description Sample 1')
+IdentityType.create(IdentityTypeName: 'Identity Type Sample 2', IdentityTypeDescription: 'Identity Type Description Sample 2')
+IdentityType.create(IdentityTypeName: 'Identity Type Sample 3', IdentityTypeDescription: 'Identity Type Description Sample 3')
+IdentityType.create(IdentityTypeName: 'Identity Type Sample 4', IdentityTypeDescription: 'Identity Type Description Sample 4')
+IdentityType.create(IdentityTypeName: 'Identity Type Sample 5', IdentityTypeDescription: 'Identity Type Description Sample 5')
+
+Source.create(SourceDescription: 'Source Description Sample 1')
+Source.create(SourceDescription: 'Source Description Sample 2')
+Source.create(SourceDescription: 'Source Description Sample 3')
+Source.create(SourceDescription: 'Source Description Sample 4')
+Source.create(SourceDescription: 'Source Description Sample 5')
+
+
