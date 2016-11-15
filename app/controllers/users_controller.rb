14c58a8ce1a197d79278b105ba3b5b0d195f2d13
donnapham <ddpham4@uh.edu>
2016-11-15 12:41:55 -0600

    DonnaPham_AdminDevelopmentPt17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_admin!, :except => [:show]
+  #before_filter :authenticate_admin!, :except => [:show]
 
   def show
     @user = User.find_by_username(params[:id])
