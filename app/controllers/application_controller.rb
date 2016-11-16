ba41e08d0bbbb6dbe2437d992eda1d7be4794232
donnapham <ddpham4@uh.edu>
2016-11-15 20:07:57 -0600

    DonnaPham_AdminDevelopmentPt22

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password,:password_confirmation, :user_group_id ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password,:password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :first_name, :last_name, :password,:password_confirmation, :user_group_id ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :first_name, :last_name, :password,:password_confirmation])
   end
 
   # private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   mount RailsAdmin::Engine => '/admin_panel', as: 'rails_admin'
 
   devise_for :users, :path_prefix => 'd'
   resources :users, :only =>[:show]
 
   match '/users',   to: 'users#index',   via: 'get'
-  match '/users/:id',     to: 'users#show',       via: 'get'
+  match '/users/:email',     to: 'users#show',       via: 'get'
 
   resources :member_groups
   resources :user_groups
